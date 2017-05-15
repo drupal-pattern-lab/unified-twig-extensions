cd945eb9a3235963dab81d37c11c423518cecaee
Salem <me@salemghoweri.com>
2017-05-15 18:08:44 -0400

    Initial commit -- adding working POC + 2 example Twig extensions to get started

---
+++ example/_twig-components/functions/link.function.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!class_exists('Drupal')) {
+  $function = new Twig_SimpleFunction(
+      'link',
+      function ($title, $url, $attributes) {
+        if (isset($attributes) && isset($attributes['class'])) {
+          $classes = join(' ', $attributes['class']);
+          return '<a href="' . $url . '" class="' . $classes . '">' . $title . '</a>';
+        } else {
+          return '<a href="' . $url . '">' . $title . '</a>';
+        }
+      },
+      array('is_safe' => array('html'))
+  );
+}
